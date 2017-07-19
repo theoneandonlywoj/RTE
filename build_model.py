317ce38690a24c2404002d7c8b3bc5cabfec24ad
arcyfelix <wor@kth.se>
2017-07-19 17:07:55 +0200

    Small changes in the model.

--- build_model.py
+++ build_model.py
@@ -1,29 +1,31 @@
-from keras.layers.core import Dense, Activation, Dropout
-from keras.layers.recurrent import LSTM
-from keras.layers.convolutional import Conv1D
+from keras.layers.core import Dense, Activation, Dropout, Flatten
+from keras.layers.recurrent import LSTM, GRU
+from keras.layers.convolutional import Convolution1D
+from keras.layers.normalization import BatchNormalization
 from keras.models import Sequential
-from keras.optimizers import Adam
+from keras.optimizers import Adam, RMSprop
 import time
 
 def build_model(features, seq_len, out):
     model = Sequential()
 
-    model.add(LSTM(100, input_shape = (seq_len, features), return_sequences=False))
-    #model.add(Dropout(0.5))
+    model.add(LSTM(100, input_shape = (seq_len, features), return_sequences = True))
+    model.add(Activation("tanh"))
+   
     
-	
-    #model.add(LSTM(10,return_sequences = True))
-
-    #model.add(LSTM(20,return_sequences = False))
-    model.add(Dropout(0.5))
-
+    model.add(Convolution1D(50, 10, border_mode='valid'))
+    model.add(Activation("relu"))
+    
+    model.add(Flatten())
+    
     model.add(Dense(units = out))
-    #model.add(Activation("linear"))
+    model.add(Activation("linear"))
 
     start = time.time()
-    adam = Adam(lr = 0.5, beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-08, decay = 0.0)
+    adam = Adam(lr = 0.25, beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-08, decay = 0.0)
 
-    model.compile(loss = "mse", optimizer = adam)
+    #model.compile(loss = "mean_absolute_percentage_error", optimizer = 'RMSprop')
+    model.compile(loss = "mean_absolute_percentage_error", optimizer = adam)
     print("> Compilation Time : ", time.time() - start)
     return model
 
